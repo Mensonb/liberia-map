6dc1602e7f192ab204b5d20c45c335638572aacf
Menson B Yormie <66823066+Mensonb@users.noreply.github.com>
2022-12-11 00:57:06 +0000

    Navbar

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Navbar from './components/Navbar'
+
+
+function App() {
+  return (
+ <>
+ <h1>
+ <img
+  src="https://flagcdn.com/lr.svg" 
+  width="150"
+  height=""
+  alt="Liberia"
+  title='Liberia Flag'
+  />
+ </h1>
+ 
+ {/* <h1>Hello World</h1> */}
+ <Navbar />
+ </>
+     
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Navbar(){
+    return(
+
+<>
+<ul>
+<li><a href="#Home">Home</a></li>
+<li><a href="#About">About</a></li>
+<li><a href="#ListOfCountries">List Of Counties</a></li>
+<li className="right"><a className="active" href="#">Sign Up</a></li>
+    
+</ul>
+
+</>
+
+    )
+}
