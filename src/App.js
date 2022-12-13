5c695e2f8e2ec04ee95a35b07aeee1b50be02448
Menson B Yormie <66823066+Mensonb@users.noreply.github.com>
2022-12-13 21:19:18 +0000

    Cards components done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar'
+import Counties from './components/Counties'
 
 
-function App() {
+export default function App() {
   return (
- <>
- <h1>
- <img
-  src="https://flagcdn.com/lr.svg" 
-  width="150"
-  height=""
-  alt="Liberia"
-  title='Liberia Flag'
-  />
- </h1>
- 
- {/* <h1>Hello World</h1> */}
+  <>
  <Navbar />
- </>
-     
-  );
-}
-
-export default App;
+ <Counties/>      
+</>
+  )}
