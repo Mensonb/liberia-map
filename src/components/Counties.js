4027fad4564bbdd274a1a8a679f5ac298048510f
Menson B Yormie <66823066+Mensonb@users.noreply.github.com>
2022-12-14 16:23:00 +0000

    new update wednesday

--- src/components/Counties.js
+++ src/components/Counties.js
@@ -1,50 +1,69 @@
 import React from "react";
 
-export default function Counties(){
-    return(
+export default function Counties() {
+    return (
 
-  <>
-  <div class="grid lg:grid-cols-4">
-    <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
-        <img class="w-full h-48"
-            src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
-            alt="product" />
-        <div class="px-6 py-4">
-            <h4 class="mb-3 text-xl font-semibold tracking-tight text-gray-800">Bomi County</h4>
-            <p class="leading-normal text-gray-700">Bomi is one of the fifteen counties of Liberia. The county is located in the northwestern part of the country. Bomi county has a surface area of 1,942 square kilometers and a population of 82,036 according to 2008 population data. County was founded in 1984, and its capital is Tubmanburg. Bomi is divided into four districts.</p>
-        </div>
-    </div>
-    <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
-        <img class="w-full h-48"
-            src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
-            alt="product" />
-        <div class="px-6 py-4">
-            <h4 class="mb-3 text-xl font-semibold tracking-tight text-gray-800">Bong County</h4>
-            <p class="leading-normal text-gray-700">Bong is one of the fifteen counties of Liberia. The county is located in the northern and central part of the country. Bong County has a surface area of 8,772 square kilometers and a population of 328,919 according to 2008 data. County was founded in 1964, and its capital is Gbarnga. Bong is divided into twelve districts.</p>
-        </div>
-    </div>
-    <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
-        <img class="w-full h-48"
-            src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
-            alt="product" />
-        <div class="px-6 py-4">
-            <h4 class="mb-3 text-xl font-semibold tracking-tight text-gray-800">Nimba County</h4>
-            <p class="leading-normal text-gray-700">Nimba is one of the fifteen counties of Liberia. The county is located in the central and northern part of the country. Nimba county has a surface area of 46,500 square kilometers and a population of 468,088 according to 2008 data. The county has the highest surface area. County was founded in 1964, and its capital is Sanniquellie. Nimba is divided into six districts.</p>
-        </div>
-    </div>
-    <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
-        <img class="w-full h-48"
-            src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
-            alt="product" />
-        <div class="px-6 py-4">
-            <h4 class="mb-3 text-xl font-semibold tracking-tight text-gray-800">Lofa County</h4>
-            <p class="leading-normal text-gray-700">Lofa is one of Liberia’s fifteen counties. The county is located in the northernmost part of the country. Lofa county has a surface area of 9,982 square kilometers and a population of 270,114 according to 2008 data. County was founded in 1964, and its capital is Voinjama. Lofa is divided into six districts.</p>
-        </div>
-    </div>
-</div>
-  
-  </>
-    
-    
-)
+        <>
+            <div className="mx-auto max-w-5xl py-8">
+
+                <div class="grid lg:grid-cols-3 gap-3">
+                    <div class="mx-3 mb-2 rounded-lg shadow-lg">
+
+                        <a href="#">
+
+                            <img class="w-full h-48"
+                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Flag_of_Bomi_County.svg/180px-Flag_of_Bomi_County.svg.png"
+                                alt="Bomi-county" title="Bomi County" />
+                        </a>
+                        <div class="px-2 py-2">
+                            <h4 class="mb-3 text-xl font-semibold tracking-tight text-gray-800">Bomi County</h4>
+                            <p class="leading-normal text-gray-700">Bomi is one of the fifteen counties of Liberia. The county is located in the northwestern part of the country. Bomi county has a surface area of 1,942 square kilometers and a population of 82,036 according to 2008 population data. County was founded in 1984, and its capital is Tubmanburg. Bomi is divided into four districts.</p>
+                        </div>
+                    </div>
+
+                    <div class="mx-4 mb-2 rounded-lg shadow-lg">
+
+                        <a href="">
+                            <img class="w-full h-48"
+                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Flag_of_Bong_County.svg/180px-Flag_of_Bong_County.svg.png"
+                                alt="Bong-County" title="Bong County" />
+                        </a>
+                        <div class="px-6 py-4">
+                            <h4 class="mb-3 text-xl font-semibold tracking-tight text-gray-800">Bong County</h4>
+                            <p class="leading-normal text-gray-700">Bong is one of the fifteen counties of Liberia. The county is located in the northern and central part of the country. Bong County has a surface area of 8,772 square kilometers and a population of 328,919 according to 2008 data. County was founded in 1964, and its capital is Gbarnga. Bong is divided into twelve districts.</p>
+                        </div>
+                    </div>
+                    <div class="mx-4 mb-2 rounded-lg shadow-lg">
+                        <a href="#">
+                            <img class="w-full h-48"
+                                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Flag_of_Nimba_County.svg/180px-Flag_of_Nimba_County.svg.png"
+                                alt="Nimba" title="Nimba County" />
+
+                        </a>
+                        <div class="px-6 py-4">
+                            <h4 class="mb-3 text-xl font-semibold tracking-tight text-gray-800">Nimba County</h4>
+                            <p class="leading-normal text-gray-700">Nimba is one of the fifteen counties of Liberia. The county is located in the central and northern part of the country. Nimba county has a surface area of 46,500 square kilometers and a population of 468,088 according to 2008 data. The county has the highest surface area. County was founded in 1964, and its capital is Sanniquellie. Nimba is divided into six districts.</p>
+                        </div>
+                    </div>
+                    <div class="mx-4 mb-2 rounded-lg shadow-lg">
+
+                        <a href="#" >
+
+                            <img class="w-full h-48"
+                                src=""
+                                alt="Lofa County" title="Lofa County" />
+                        </a>
+
+                        <div class="px-6 py-4">
+                            <h4 class="mb-3 text-xl font-semibold tracking-tight text-gray-800">Lofa County</h4>
+                            <p class="leading-normal text-gray-700">Lofa is one of Liberia’s fifteen counties. The county is located in the northernmost part of the country. Lofa county has a surface area of 9,982 square kilometers and a population of 270,114 according to 2008 data. County was founded in 1964, and its capital is Voinjama. Lofa is divided into six districts.</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </>
+
+
+    )
 }
